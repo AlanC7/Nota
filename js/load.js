890e3ce1ad637e36270f4b47725769be483ce908
AlanC7 <81393852+AlanC7@users.noreply.github.com>
2022-06-28 18:26:48 -0500

    Add files via upload

--- js/load.js
+++ js/load.js
@@ -1,12 +1,11 @@
 window.addEventListener('load',()=>
 {
     var loader=document.querySelector('.loading')
-    var carrusel=document.querySelector('#carrusel').classList.remove("ocultarObjetos")
-    var scrollbody=document.querySelector('#bodyscroll').classList.remove("ocultarScroll")
-    var barraNav=document.querySelector('#barraNav').classList.remove("ocultarObjetos")
-    var linea=document.querySelector('#linea').classList.remove("ocultarObjetos")
+    //var navegacion=document.querySelector('.SP')
     loader.style.opacity= '0%' 
     loader.style.transition= 'all 1s' 
-    loader.style.display='none'
-    
+    loader.style.visibility='hidden' 
+    var scrollbody=document.querySelector('#bodyscroll').classList.remove("ocultarScroll")
+        
 })
+
